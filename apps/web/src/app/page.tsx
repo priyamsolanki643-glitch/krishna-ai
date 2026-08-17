7cbaad3094b8fb6f928085a58d29d49b5c456a54
priyamsolanki643-glitch <sunitacool40@gmail.com>
2026-08-17 20:42:16 +0530

    chore: removed stars, set to pure 100% pitch black void

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,15 +1,7 @@
-import { StarfieldBackground } from "../components/StarfieldBackground";
-
 export default function Home() {
   return (
-    <main className="relative min-h-screen w-full bg-black text-white flex flex-col items-center justify-center p-4 overflow-hidden">
-      {/* 60fps Aesthetic Moving Deep-Space Stars Background */}
-      <StarfieldBackground />
-
-      {/* Foreground Container for Onboarding */}
-      <div className="relative z-10 w-full max-w-md mx-auto flex flex-col items-center justify-center text-center">
-        {/* Ready for Onboarding elements */}
-      </div>
+    <main className="min-h-screen w-full bg-black text-white flex flex-col items-center justify-center p-4">
+      {/* 100% Pure Pitch Black Canvas */}
     </main>
   );
 }
